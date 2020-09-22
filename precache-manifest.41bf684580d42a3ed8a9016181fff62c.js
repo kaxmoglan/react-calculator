f535697690abed4f1c6961279084fecffe9bd1ed
Maximilian Coghlan <max.coghlan@icloud.com>
2020-09-22 14:23:07 +0100

    Updates

---
+++ precache-manifest.41bf684580d42a3ed8a9016181fff62c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ce119445cef52cc29a4c5aeeaa8cd8",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "f344c3b7daea626c443d",
+    "url": "/react-calculator/static/css/main.ef91d9da.chunk.css"
+  },
+  {
+    "revision": "ecada51e1a3334c42b99",
+    "url": "/react-calculator/static/js/2.e6f076d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.e6f076d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f344c3b7daea626c443d",
+    "url": "/react-calculator/static/js/main.70d634ee.chunk.js"
+  },
+  {
+    "revision": "14453d16db6c0e44d6da",
+    "url": "/react-calculator/static/js/runtime-main.7b47f775.js"
+  }
+]);
